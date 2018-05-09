10217a23901a0f1c626c38008bcfe39ffb61e7c7
Ali Huda <alihuda1992@gmail.com>
2018-05-09 16:41:53 -0400

    updating

--- server.js
+++ server.js
@@ -1,10 +1,38 @@
-const express = require('express');
-const nodemon = require('nodemon');
+var express = require("express");
+var bodyParser = require("body-parser");
+var path = require('path');
 
 var app = express();
- 
-app.get('/', function (req, res) {
-  res.send('Hello World')
-})
- 
-app.listen(3000)
+var PORT = process.env.PORT || 8080;
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+
+
+
+
+
+
+
+// Get route to /survey which should display survey page
+app.get('/survey', function(req, res) {
+  res.sendFile('survey.html', { root: path.join(__dirname, '../friendfinder/app/public') });
+});
+
+
+// a catch all route that leads to home.html
+app.get('/', function(req, res) {
+  res.sendFile('home.html', { root: path.join(__dirname, '../friendfinder/app/public') });
+});
+
+
+
+// require("./app/routing/apiRoutes")(app);
+// require("./app/routing/htmlRoutes")(app);
+
+
+
+app.listen(PORT, function () {
+  console.log("listening on PORT" + PORT);
+});
